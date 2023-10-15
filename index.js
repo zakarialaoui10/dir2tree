668932fc6bb01af3e8cffd8a074c562676d57653
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-15 14:44:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*
+Developped by zakaria elaloui
+Github : https://github.com/zakarialaoui10
+*/
 const Dir2Tree=require("./src/dir2tree.js")
 const dir2tree = (root, options, callbacks) => new Dir2Tree(root, options, callbacks);
 module.exports=dir2tree
