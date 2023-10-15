8ef8cc9dfdd6f179f5fc5b5d11ad6e4201b340eb
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-15 18:16:03 +0100

    Update index.js

--- Examples/0- Basic /index.js
+++ Examples/0- Basic /index.js
@@ -1,1 +1,18 @@
-
+const fs = require('fs');
+const path = require('path');
+const dir2tree=require("../index.js")
+const ROOT = path.join(__dirname,'.',"Js");
+const TARGET = path.join(__dirname, 'generated2.json');
+//const W=watch(ROOT).start()
+const callbacks = [
+    a=(filePath, fileInfo) => {
+      fileInfo.content2=fs.readFileSync(filePath, 'utf8');
+    },
+  ]
+const tree = dir2tree(ROOT,{fileContent:true,sortBy:"created"},callbacks);
+tree.write("gen.json")
+//fs.writeFileSync(TARGET,JSON.stringify(tree,null,2));
+//console.log(1111111111)
+//console.log(tree);
+//console.log(1111111111111)
+//console.log(asm)
