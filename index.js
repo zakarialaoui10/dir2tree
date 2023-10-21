fa6f96b675f6a8b8361f2fa8b961e44e9fdc95b0
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 13:34:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 Developped by zakaria elaloui
 Github : https://github.com/zakarialaoui10
 */
+console.log("start")
 const Dir2Tree=require("./src/dir2tree.js")
 const dir2tree = (root, options, callbacks) => new Dir2Tree(root, options, callbacks);
 const ROOT = path.join(__dirname,'.',"Examples");
 const TARGET = path.join(__dirname, 'generated2.json');
 const tree = dir2tree(ROOT,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]});
 tree.write("gen.json")
+consol.log("end")
 module.exports=dir2tree
 
