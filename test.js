d30c1caae3bb0a4eac98f0fafc0b03e9dbecbd5c
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 21:20:05 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const { Octokit } = require('@octokit/rest');
 
+async function main() {
+  // Read a file from your master repository
+  const fileData = fs.readFileSync('path/to/file.txt', 'utf-8');
+
+  // Use the GitHub API to interact with the slave repository
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN_SLAVE, // Use a secret with the token for the slave repo
+  });
+
+  // Create, update, or delete files/folders in the slave repository as needed
+  // Example:
+  await octokit.repos.createOrUpdateFile({
+    owner: 'slave-repo-owner',
+    repo: 'slave-repo-name',
+    path: 'path/in/slave-repo/newfile.txt',
+    message: 'Create new file',
+    content: Buffer.from(fileData).toString('base64'),
+  });
+}
+
+main()
+  .catch((error) => {
+    console.error('Error:', error);
+  });
