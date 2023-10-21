4db45023d673132c8063c1132db9049061bcb0d4
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 14:38:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 Developped by zakaria elaloui
 Github : https://github.com/zakarialaoui10
 */
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'example.txt');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+  } else {
+    console.log('File content:', data);
+  }
+});
+/*
 console.log("start")
 const Dir2Tree=require("./src/dir2tree.js")
 console.log(Dir2Tree)
@@ -11,5 +24,6 @@ const TARGET = path.join(process.cwd(), 'generated2.json');
 const tree = dir2tree(ROOT,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]});
 tree.write("gen.json")
 consol.log("end")
+*/
 module.exports=dir2tree
 
