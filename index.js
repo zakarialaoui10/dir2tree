d65f27a742c419f93e80bcefd7407c06ec73adb9
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 14:47:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ Github : https://github.com/zakarialaoui10
 const fs = require('fs');
 const path = require('path');
 const Dir2Tree=require("./src/dir2tree.js")
-const dir2tree = (root, options, callbacks) => new Dir2Tree(root, options, callbacks);
+const dir2tree = (root, options, callbacks=[]) => new Dir2Tree(root, options, callbacks);
 const ROOT = path.join(__dirname,'.',"Examples");
 const TARGET = path.join(__dirname, 'generated2.json');
-const tree = dir2tree(ROOT,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]},[]);
+const tree = dir2tree(ROOT,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]});
 tree.write("gen.json")
 console.log("end")
 module.exports=dir2tree
