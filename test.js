02068ac9dc3d43030c0c5d233f7baeb32a38e215
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 21:47:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+/*
 const fs = require('fs');
 const path = require('path');
 
@@ -10,3 +11,26 @@ fs.readFile(filePath, 'utf8', (err, data) => {
     console.log('File content:', data);
   }
 });
+*/
+const { Octokit } = require('@octokit/rest');
+
+async function readSlaveFile() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN_MASTER, // Use your GitHub token secret from the master repository.
+  });
+
+  try {
+    const response = await octokit.repos.getContent({
+      owner: 'slave-repo-owner',
+      repo: 'slave-repo-name',
+      path: 'path/in/slave-repo/example.txt',
+    });
+
+    const fileContent = Buffer.from(response.data.content, 'base64').toString();
+    console.log('File Content:', fileContent);
+  } catch (error) {
+    console.error('Error reading file:', error.message);
+  }
+}
+
+readSlaveFile();
