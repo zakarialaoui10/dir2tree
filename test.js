f57cf1c689d3ae0a705ded69307cd3e1e2ffabcc
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 21:38:36 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,12 @@
 const fs = require('fs');
-const { Octokit } = require('@octokit/rest');
+const path = require('path');
 
-async function main() {
-  // Read a file from your master repository
-  const fileData = fs.readFileSync('path/to/file.txt', 'utf-8');
+const filePath = path.join(__dirname, 'example.txt');
 
-  // Use the GitHub API to interact with the slave repository
-  const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN_SLAVE, // Use a secret with the token for the slave repo
-  });
-
-  // Create, update, or delete files/folders in the slave repository as needed
-  // Example:
-  await octokit.repos.createOrUpdateFile({
-    owner: 'slave-repo-owner',
-    repo: 'slave-repo-name',
-    path: 'path/in/slave-repo/newfile.txt',
-    message: 'Create new file',
-    content: Buffer.from(fileData).toString('base64'),
-  });
-}
-
-main()
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the file:', err);
+  } else {
+    console.log('File content:', data);
+  }
+});
