dbf13a05dabc9c2047f680db22bb809a875c391a
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 14:40:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ Github : https://github.com/zakarialaoui10
 */
 const fs = require('fs');
 const path = require('path');
+const Dir2Tree=require("./src/dir2tree.js")
+const dir2tree = (root, options, callbacks) => new Dir2Tree(root, options, callbacks);
+const ROOT = path.join(__dirname,'.',"Examples");
 
 const filePath = path.join(__dirname, 'example.txt');
 
@@ -16,10 +19,7 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 });
 /*
 console.log("start")
-const Dir2Tree=require("./src/dir2tree.js")
 console.log(Dir2Tree)
-const dir2tree = (root, options, callbacks) => new Dir2Tree(root, options, callbacks);
-const ROOT = path.join(process.cwd(),'.',"Examples");
 const TARGET = path.join(process.cwd(), 'generated2.json');
 const tree = dir2tree(ROOT,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]});
 tree.write("gen.json")
