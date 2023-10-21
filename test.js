f754dd74042d61440d6e43bb7d519627d7432d90
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 21:56:40 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, 'example.txt');
-
+ 
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the file:', err);
