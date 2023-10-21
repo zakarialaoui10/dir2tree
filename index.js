6ab6faa26bd51da8a68a328d5542330660afdc91
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-21 22:28:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const fs = require('fs');
 const path = require('path');
 const Dir2Tree=require("./src/dir2tree.js")
 const dir2tree = (root, options, callbacks=[]) => new Dir2Tree(root, options, callbacks);
-const ROOT = path.join(__dirname,'.',"Examples");
+//const ROOT = path.join(__dirname,'.',"Examples");
+const ROOT="/home/runner/work/_actions/zakarialaoui10/action-test/main"
 const TARGET = path.join(__dirname, 'generated2.json');
 console.log({ROOT,TARGET})
 const tree = dir2tree(__dirname,{fileContent:true,sortBy:"extension",skipFile:["hh.txt"]});
