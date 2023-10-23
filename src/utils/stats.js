0bb61fe950f81eb6cb59befe250eaec1cd4b69f9
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-23 18:55:44 +0100

    Create stats.js

---
+++ src/utils/stats.js
@@ -0,0 +1,10 @@
+function file_metadata(filePath) {
+    const stats = fs.statSync(filePath);
+    const metadata = {
+      created: stats.birthtime,
+      modified: stats.mtime,
+      permissions: stats.mode,
+    };
+    return metadata;
+  }
+module.exports={file_metadata}
