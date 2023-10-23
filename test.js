f49a4697f5d6060ebc82ebd8aa5eccf1c84a947e
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-23 23:59:56 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-const dir2tree=require("dir2tree")
 const fs=require("fs")
 const path=require("path")
-
+const Dir2Tree=require("./src/dir2tree.js")
+const dir2tree = (root, options, callbacks=[]) => new Dir2Tree(root, options, callbacks);
 const ROOT = path.join(__dirname,'.',"Articles");
 const TARGET = path.join(__dirname,"Target");
 
