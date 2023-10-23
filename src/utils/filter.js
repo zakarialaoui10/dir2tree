635e49978a9d605c6279722381521c930097fe2f
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-23 19:03:13 +0100

    Update filter.js

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,1 +1,14 @@
-
+const path=require("path");
+const {isDirectory}=require("./general.js");
+const {should_skip_file}=require("./skip.js")
+function filter_files(files) {
+    return files.filter((file) => {
+      if (isDirectory(path.join(this.root, file))) {
+        return true; // Skip directories
+      }
+      const filePath = path.join(this.root, file);
+      const shouldSkip = should_skip_file.call(this, filePath);
+      return !shouldSkip;
+    });
+  }
+module.exports={filter_files}
