c5aada8c10aa698f48cd16f160b953ae7b79fee1
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-24 22:53:46 +0100

    Update skip.js

--- src/utils/skip.js
+++ src/utils/skip.js
@@ -2,25 +2,19 @@ const path = require("path");
 function should_skip_file(filePath) {
     const normalizedPath = path.normalize(filePath);
     if (
-      this?.options?.skipFolder?.includes(path.basename(normalizedPath)) ||
-      this?.options?.skipFile?.includes(path.basename(normalizedPath)) ||
-      this?.options?.skipExtension?.includes(
+      this?.options?.skip?.folder?.includes(path.basename(normalizedPath)) ||
+      this?.options?.skip?.file?.includes(path.basename(normalizedPath)) ||
+      this?.options?.skip?.extension?.includes(
         path.extname(normalizedPath).slice(1)
       )
     )return true;
     return false;
   }
 function should_skip_folder(filePath) {
-    if (typeof filePath !== 'string') {
-      return false;
-    }
-  
-    const normalizedPath = path.normalize(filePath);
-  
-    if (this?.options?.skipFolder?.includes(path.basename(normalizedPath))) {
-      return true;
-    }
-  
-    return false;
+  if (typeof filePath !== 'string') return false;
+  const normalizedPath = path.normalize(filePath);
+  if (this?.options?.skipFolder?.includes(path.basename(normalizedPath)))
+    return true;
+  return false;
   }
 module.exports={should_skip_file,should_skip_folder}
