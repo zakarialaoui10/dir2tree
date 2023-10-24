d8c59ee49d8f3f60053adcd32611fbaa134b48a0
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-24 23:16:16 +0100

    Update callbacks.js

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -2,9 +2,7 @@
 
 // Marked
 const markdown_to_html=(filePath,fileInfo)=>{
-  if(marked){
-  return fileInfo.extension==="md"?marked.parse(fs.readFileSync(filePath,"utf-8"))
-    }
+    if(marked)fileInfo.html=marked.parse(fs.readFileSync(filePath,"utf-8"));
 }
 // Highlight
 const highlight=()=>
