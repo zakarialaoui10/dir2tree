78404990d52338ff304c91deca6bf4d17bfbd425
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-24 23:24:51 +0100

    Update callbacks.js

--- src/utils/callbacks.js
+++ src/utils/callbacks.js
@@ -2,13 +2,16 @@ const fs=require("fs")
 const path=require("path")
 // Native 
 
-// Marked
+// Marked.js
 const markdown_to_html=(filePath,fileInfo)=>{
     const extension = path.basename(filePath).split(".")[1];
     if(marked && extension=="md")fileInfo.html=marked.parse(fs.readFileSync(filePath,"utf-8"));
 }
-// Highlight
+// Highlight.js
 const highlight=()=>
-// Crypto
+// Crypto.js
 const crypt=()=>{}
 const decrypt=()=>{}
+// Sheet.js
+const xls2json=()={}
+const json2xls=()={}
