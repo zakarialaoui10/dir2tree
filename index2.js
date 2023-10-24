853fe0e0734e69ba58ef9f3e785a79de187cf09e
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-24 19:03:06 +0100

    Update index2.js

--- index2.js
+++ index2.js
@@ -2,17 +2,16 @@ const fs = require('fs');
 
 try {
   const data = fs.readFileSync('user.json', 'utf8');
-  console.log(data);
-} catch (error) {
-  console.error('Error reading the file:', error);
-}
-
-const content = 'Hello, World! ' + Math.random();
-fs.writeFile('hello.txt', content, (err) => {
+fs.writeFile('hello.txt', data, (err) => {
   if (err) {
     console.error('An error occurred:', err);
   } else {
     console.log(`File "hello.txt" has been created with the content: ${content}`);
   }
 });
+
+} catch (error) {
+  console.error('Error reading the file:', error);
+}
+
 
