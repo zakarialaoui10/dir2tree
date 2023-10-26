80c215fea417472bd8a74e11259abfe0696a0935
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-26 11:07:30 +0100

    fix automation/index.js

--- Automation/index.js
+++ Automation/index.js
@@ -1,15 +1,15 @@
 const fs=require("fs")
 const path=require("path")
 const dir2tree=require("dir2tree")
-const ROOT = path.join(process.cwd(),'..',"Articles");
-const TARGET = path.join(process.cwd(),"..");
+const ROOT = path.join(process.cwd(),'.');
+const TARGET = path.join(process.cwd(),".");
 console.log({ROOT,TARGET})
 const MyTree = dir2tree(ROOT,{
   fileContent:true,
   sortBy:"extension",
   skipFile:["ger.md"],
   skipFolder:["to be skipped"],
-  skipExtension:["sd"],
+  skipExtension:["json"],
 });
 
 console.log(MyTree.tree)
