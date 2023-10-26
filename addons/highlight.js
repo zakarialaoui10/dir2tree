d484c0a6ea771d9c25ca1b8796e8980f6a64ba81
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-26 12:10:24 +0100

    add highlight.js

---
+++ addons/highlight.js
@@ -0,0 +1,4 @@
+const {require_if_installed}=require("./utils.js");
+const path=require("path");
+const fs=require("fs");
+const hljs=require_if_installed("highlight.Js");
--- addons/marked.js
+++ addons/marked.js
@@ -1,6 +1,6 @@
 const {require_if_installed}=require("./utils.js");
 const path=require("path");
-const fs=require("fs")
+const fs=require("fs");
 const marked=require_if_installed("marked");
 const markdown_to_html=(filePath,fileInfo)=>{
   const extension = path.basename(filePath).split(".")[1];
