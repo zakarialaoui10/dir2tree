590d80de5492437a2fcf4384564c34f4b12f830a
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-26 13:52:04 +0100

    update addons

--- addons/callbacks/Highlight.js
+++ addons/callbacks/Highlight.js
@@ -1,4 +1,4 @@
-const {require_if_installed}=require("./utils.js");
+const {require_if_installed}=require("../utils/index.js");
 const path=require("path");
 const fs=require("fs");
 const hljs=require_if_installed("highlight.Js");
---
+++ addons/index.js
@@ -0,0 +1,1 @@
+module.exports={require_if_installed}
