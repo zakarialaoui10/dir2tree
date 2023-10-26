ab2469992e851703a2585c340520e861b6357a4a
zakarialaoui10 <zakarialaoui10@gmail.com>
2023-10-26 12:21:36 +0100

    rename marked

--- Automation/index.js
+++ Automation/index.js
@@ -1,7 +1,7 @@
 const fs=require("fs");
 const path=require("path");
 const dir2tree=require("dir2tree");
-const {markdown_to_html}=require("../addons/marked.js")
+const {markdown_to_html}=require("../addons/Marked.js")
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
 const MyTree = dir2tree(ROOT,{
