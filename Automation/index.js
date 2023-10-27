8f1636ac0b2af30983213111569121790c011d5b
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-27 19:29:41 +0100

    Update index.js

--- Automation/index.js
+++ Automation/index.js
@@ -1,7 +1,7 @@
 const fs=require("fs");
 const path=require("path");
 const dir2tree=require("dir2tree");
-const {markdown_to_html}=require("../addons/Marked.js")
+const {markdown_to_html}=require("../addons/index.js")
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
 const MyTree = dir2tree(ROOT,{
