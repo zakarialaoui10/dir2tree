47902da532c6b1793529df6cea95eb3c2b6b176b
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-28 13:08:49 +0100

    Update index.js

--- Automation/index.js
+++ Automation/index.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 const path=require("path");
-const dir2tree=require("dir2tree");
+const dir2tree=require("./dist/index.cjs");
 //const {markdown_to_html}=require("../addons/index.js")
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
