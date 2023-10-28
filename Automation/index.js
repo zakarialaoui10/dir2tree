3d9a3e9f2ae279ebdd3d4dfea21cc9ccedfcb739
ZAKARIA ELALAOUI <63521765+zakarialaoui10@users.noreply.github.com>
2023-10-28 13:04:55 +0100

    Update index.js

--- Automation/index.js
+++ Automation/index.js
@@ -1,7 +1,7 @@
-/*const fs=require("fs");
+const fs=require("fs");
 const path=require("path");
 const dir2tree=require("dir2tree");
-const {markdown_to_html}=require("../addons/index.js")
+//const {markdown_to_html}=require("../addons/index.js")
 const ROOT = path.join(process.cwd(),'.');
 const TARGET = path.join(process.cwd(),".");
 const MyTree = dir2tree(ROOT,{
@@ -10,10 +10,9 @@ const MyTree = dir2tree(ROOT,{
   skipFile:["ger.md"],
   skipFolder:[".git","node_modules"],
   skipExtension:["json"],
-},[markdown_to_html]);
+},[]);
 
 console.log(MyTree.tree)
 MyTree.write(TARGET,"generated.json")
-*/
 import dir2tree from "dir2tree";
 console.log(dir2tree)
